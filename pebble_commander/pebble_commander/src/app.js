2d79ec3b75798230017153ad00687a07a984f0da
Colin Murphy <mrtux@riseup.net>
2015-12-16 20:56:21 -0600

    Add the pebble app code + readme updates

---
+++ pebble_commander/pebble_commander/src/app.js
@@ -0,0 +1,116 @@
+var UI = require('ui');
+var ajax = require('ajax');
+var Settings = require('settings');
+
+// APP SETTINGS
+//var CONTROL_URL = "http://df87ad53.ngrok.io/";
+//var AUTH_KEY = "cbr674";
+
+// Set a configurable with the open callback
+Settings.config(
+	{ url: 'https://c0decat.github.io/pebble-commander/settings/' },
+	function(e) {
+		console.log('opening configurable');
+	},
+	function(e) {
+		console.log('closed configurable');
+	}
+);
+
+var CONTROL_URL = Settings.option('server'); 
+var AUTH_KEY = Settings.option('port');
+var OUTPUT_MODE = Settings.option('commandoutput');
+
+// Initial window
+var initWindow = new UI.Card({
+	title: "Pebble Commander",
+	scrollable: true,
+	style: 'small',
+	body: "Getting commands list..."
+});
+initWindow.show();
+
+
+if (!CONTROL_URL || !AUTH_KEY) {
+	// App not configured
+	initWindow.title("App not configured");
+	initWindow.body("Enter in the server IP and password on your phone. When done, restart this app.");
+	initWindow.backgroundColor('red');
+  
+	//when you press the select button, restart the app
+	initWindow.on('select', function(event) {
+		runApp();
+	});
+}
+else
+{
+	// all is good, you can run the app
+	runApp();
+}
+
+
+function runApp() {
+	ajax({url: 'http://' + CONTROL_URL + "/send_json/" + AUTH_KEY, type: 'json'},
+		function(json) {
+			// Data retrieval worked, hide this and show the menu!
+			var commandMenu = new UI.Menu({
+				backgroundColor: 'white',
+				textColor: 'black',
+				highlightBackgroundColor: 'blue',
+				highlightTextColor: 'white',
+				sections: [{
+					title: 'Commands',
+					items: json
+				}]
+			});
+			commandMenu.show();
+			initWindow.hide();
+		
+			// Add a click listener for select button click
+			commandMenu.on('select', function(event) {
+			// Show a card showing that the command was executed
+			var detailCard = new UI.Card({
+				backgroundColor: 'white',
+				textColor: 'black',
+				style: 'small',
+				scrollable: true,
+				title: json[event.itemIndex].title,
+				subtitle: "Sending command to server",
+				body: ""
+			});
+
+			// Show the new Card
+			detailCard.show();
+
+			ajax({url: 'http://' + CONTROL_URL + "/exec/" + AUTH_KEY + "/" + json[event.itemIndex].id, type: 'plain'},
+				function (data) {
+					detailCard.subtitle("Command sent");
+					if (OUTPUT_MODE === true) {
+						detailCard.body(data);
+					}
+					else
+					{
+						detailCard.body("");
+					}
+					detailCard.backgroundColor("green");
+					
+					// hide this after 2 seconds and return to the menu
+					setTimeout(function() {detailCard.hide(); commandMenu.show();}, 2000);
+				},
+				function (error) {
+					detailCard.title("Data retrieval failed");
+					detailCard.body(error);
+					detailCard.subtitle("");
+					detailCard.backgroundColor('red');
+				});
+			});
+		},
+		function(error) {
+			console.log('Ajax failed: ' + error);
+			initWindow.title('Data retrieval failed');
+			initWindow.body(error);
+			initWindow.backgroundColor('red');
+			initWindow.textColor('black');
+		}
+	);
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
-# Pebble Commander backend
+# Pebble Commander
 
-This is the python backend for the pebble app "Python Commander." It's written in Python and uses the Flask microframework. For the pebble app itself, see [Pebble Commander](https://github.com/c0decat/pebble-commander).
+Pebble Commander is an app for the Pebble smartwatch that turns your Pebble into a remote control for your computer. You can use it to launch your favorite programs, or use it for automation with your own scripts.
+
+## Backend server installation
+
+To install the server (server.py) you will need to install the following modules:
+
+* Flask (`pip --user install flask`)
+
+### Configuring the backend
+
+Configuration is done in `settings.json`. Documentation on this will be done later.
+
+## Pebble app
+
+The pebble app is not currently on the pebble app store at this time. In the meantime you'll have to import it into [CloudPebble](https://cloudpebble.net). I'll get the app on the app store ASAP.
