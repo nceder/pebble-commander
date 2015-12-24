e49708b483c309bba58e74ebf2101f2b249352cb
Colin Murphy <mrtux@riseup.net>
2015-12-23 20:03:47 -0600

    App v1.2

---
+++ commander/src/app.js
@@ -0,0 +1,189 @@
+var UI = require('ui');
+var ajax = require('ajax');
+var Settings = require('settings');
+
+
+// get pebble revision
+if(Pebble.getActiveWatchInfo) {
+	var watchinfo= Pebble.getActiveWatchInfo();
+	var platform=watchinfo.platform;
+} else {
+	platform="aplite";
+}
+
+
+// Set a configurable with the open callback
+Settings.config(
+	{ url: 'https://c0decat.github.io/pebble-commander/settings/' },
+	function(e) {
+		console.log('opening configurable');
+	},
+	function(e) {
+		console.log('closed configurable');
+	}
+);
+
+
+var CONTROL_URL = Settings.option('server'); 
+var AUTH_KEY = Settings.option('authkey');
+var OUTPUT_MODE = Settings.option('commandoutput');
+
+var APP_VERSION = "1.2";
+var ABOUT_TEXT = "Author: Colin Murphy (mrtux@riseup.net)\n\nWebsite: www.mrtux.org/projects/commander";
+
+// Initial window
+var initWindow = new UI.Card({
+	title: "Pebble Commander",
+	scrollable: true,
+	style: 'small',
+	body: "Getting commands list..."
+});
+initWindow.show();
+
+
+if (!CONTROL_URL || !AUTH_KEY) {
+	// App not configured
+	initWindow.title("App not configured");
+	initWindow.body("Enter in the server IP and password on your phone. When done, restart this app.");
+	
+	if (platform == 'basalt') {
+		initWindow.backgroundColor('red');
+	}
+  
+	//when you press the select button, restart the app
+	initWindow.on('select', function(event) {
+		runApp();
+	});
+}
+else
+{
+	// all is good, you can run the app
+	runApp();
+}
+
+
+function runApp() {
+	ajax({url: 'http://' + CONTROL_URL + "/send_json/" + AUTH_KEY, type: 'json'},
+		function(json) {
+			// Data retrieval worked, hide this and show the menu!
+			var commandMenu = new UI.Menu({
+				backgroundColor: '#AAAAAA',
+				textColor: 'black',
+				highlightBackgroundColor: 'blue',
+				highlightTextColor: 'white',
+				sections: [{
+					title: 'Commands',
+					items: json
+				},{
+					title: 'Commander',
+					items: [
+						{"title": "Connection info"},
+						{"title": "About", "subtitle": "Version " + APP_VERSION}
+					]
+				}]
+			});
+			commandMenu.show();
+			initWindow.hide();
+		
+			// Add a click listener for select button click
+			commandMenu.on('select', function(event) {
+				console.log('Selected item #' + event.itemIndex + ' of section #' + event.sectionIndex);
+				console.log('The item is titled "' + event.item.title + '"');
+				
+				// If the item selected is in the FIRST list, it is a command.
+				if (event.sectionIndex === 0 ) {
+					// Show a card showing that the command was executed
+					var detailCard = new UI.Card({
+						backgroundColor: 'white',
+						textColor: 'black',
+						style: 'small',
+						scrollable: true,
+						title: json[event.itemIndex].title,
+						subtitle: "Sending command to server",
+						body: ""
+					});
+
+					// Show the new Card
+					detailCard.show();
+
+					ajax({url: 'http://' + CONTROL_URL + "/exec/" + AUTH_KEY + "/" + json[event.itemIndex].id, type: 'plain'},
+						function (data) {
+							detailCard.subtitle("Command sent");
+							if (OUTPUT_MODE === true) {
+								detailCard.body(data);
+							}
+							else
+							{
+								detailCard.body("");
+							}
+							if (platform == 'basalt') {
+								detailCard.backgroundColor('green');
+							}
+
+							// hide this after 2 seconds and return to the menu
+							setTimeout(function() {detailCard.hide(); commandMenu.show();}, 2000);
+						},
+						function (error) {
+							detailCard.title("Data retrieval failed");
+							detailCard.body(error);
+							detailCard.subtitle("");
+							if (platform == 'basalt') {
+								detailCard.backgroundColor('red');
+							}
+						}
+					);
+				}
+				else
+				{
+					// selection index isn't 0 so this is a different type :)
+					
+					var infoMenu = new UI.Menu({
+						backgroundColor: '#AAAAAA',
+						textColor: 'black',
+						highlightBackgroundColor: 'blue',
+						highlightTextColor: 'white',
+					});
+					
+					if (event.itemIndex === 0) {
+						// an id if 0 is the connection settings card
+						infoMenu.sections([{
+							title: "Connection info",
+							items: [
+								{"title": "Server", "subtitle": CONTROL_URL},
+								{"title": "Auth key", "subtitle": AUTH_KEY},
+							]
+						}]);
+						infoMenu.show();
+					}
+					else
+					{
+						// an id of 1 is the about app card
+						var aboutCard = new UI.Card({
+							style: "small",
+							scrollable: true,
+							title: ">_ Commander",
+							subtitle: "Version " + APP_VERSION,
+							body: ABOUT_TEXT
+						});
+						if (platform == 'basalt') {
+							aboutCard.titleColor("#00AA00");
+							aboutCard.backgroundColor("#AAAAAA");
+							aboutCard.bodyColor("black");
+							aboutCard.subtitleColor("#555555");
+						}
+						aboutCard.show();
+					}
+				}
+			});
+		},
+		function(error) {
+			console.log('Ajax failed: ' + error);
+			initWindow.title('Data retrieval failed');
+			initWindow.body(error);
+			if (platform == 'basalt') {
+				initWindow.backgroundColor('red');
+				initWindow.textColor('black');
+			}
+		}
+	);
+}
